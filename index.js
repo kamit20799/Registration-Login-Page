8b55f100510ad94ef286c8b28571e4ac63a12d4d
Amit Kumar <kumaramit20799@gmail.com>
2022-11-18 02:37:35 +0530

    new code updated

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-let container = document.getElementById('container')
+// // let container = document.getElementById('container')
 
-toggle = () => {
-	container.classList.toggle('sign-in')
-	container.classList.toggle('sign-up')
-}
+// // toggle = () => {
+// // 	container.classList.toggle('sign-in')
+// // 	container.classList.toggle('sign-up')
+// // }
 
-setTimeout(() => {
-	container.classList.add('sign-in')
-}, 200)
+// // setTimeout(() => {
+// // 	container.classList.add('sign-in')
+// // }, 200)
+
+// <div class="login-box">
+//   <h2>Glow Button</h2>
+//   <h3>Login Form</h3>
+//   <form>
+//     <div class="user-box">
+//       <input type="text" name="" required="">
+//       <label>Username</label>
+//     </div>
+//     <div class="user-box">
+//       <input type="password" name="" required="">
+//       <label>Password</label>
+//     </div>
+//     <a href="#">
+//       <span></span>
+//       <span></span>
+//       <span></span>
+//       <span></span>
+//       Submit
+//     </a>
+//   </form>
+// </div>
+// <div class="lowerDiv">
+//   <h4>Original form found here.<h4>
+//     <p>https://codepen.io/soufiane-khalfaoui-hassani/pen/LYpPWda</p>
+//     <p>I just chnaged some of the code and centering.</p>
+//     <p>Thanks Soufiane Khalfaoui HaSsani for posting this.</p>
+//     </div>
